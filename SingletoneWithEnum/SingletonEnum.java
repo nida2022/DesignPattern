ff9ed5cbd82b567ce380cba13dfeb12fcf882d26
sweety bhagat <sweety.bhagat89@gmail.com>
2022-07-29 22:17:34 +0530

    singlon with enum

---
+++ SingletoneWithEnum/SingletonEnum.java
@@ -0,0 +1,30 @@
+package Singleton.DesignPattern.SingletoneWithEnum;
+
+
+enum Singleton{
+    Instance;
+    int val;
+
+    int getVal(){
+        return val;
+    }
+    void setVal(int val){
+        this.val = val;
+
+    }
+}
+public class SingletonEnum {
+
+
+    public static void main(String[] args) {
+        Singleton a = Singleton.Instance;
+        a.setVal(2);
+        System.out.println(a.hashCode());
+        Singleton a2 = Singleton.Instance;
+        a2.setVal(3);
+
+        System.out.println(a2.hashCode());
+
+
+    }
+}
