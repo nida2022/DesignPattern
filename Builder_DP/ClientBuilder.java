7a8a3729220420a26a83ce8fd8e18a12e4e2fb53
sweety bhagat <sweety.bhagat89@gmail.com>
2022-08-02 12:29:23 +0530

    pushing builder

--- Builder_DP/ClientBuilder.java
+++ Builder_DP/ClientBuilder.java
@@ -1,2 +1,14 @@
-package Singleton.DesignPattern.Builder_DP;public class ClientBuilder {
+package Singleton.DesignPattern.Builder_DP;
+
+public class ClientBuilder {
+
+    public static void main(String[] args) {
+        HouseConstruction normalhouse = HouseConstruction.getBuilder()
+                .setRoof("CementedRoof")
+                .setDoors(5)
+                .setFloor("marble Floors")
+                .build();
+
+        System.out.println(normalhouse.getfloor());
+    }
 }
--- Builder_DP/HouseConstruction.java
+++ Builder_DP/HouseConstruction.java
@@ -1,22 +1,53 @@
 package Singleton.DesignPattern.Builder_DP;
 
-public class UserExam {
+public class HouseConstruction {
 
-    int math;
-    int science;
-    int english;
-    int history;
+    String floor;
+    String roof;
+    int doors;
 
-    private  UserExam(){};
 
-    public class USerExamBuilder{
+    String getfloor(){
+        return floor;
+    }
+    private  HouseConstruction(){};
 
-        int math;
-        int science;
-        int english;
-        int history;
+    public static HouseConstructionBuilder getBuilder(){
+        return new HouseConstructionBuilder();
+    }
+    public static class HouseConstructionBuilder{
 
-        
+        String floor;
+        String roof;
+        int doors;
 
+        public HouseConstructionBuilder setFloor(String floor) {
+            this.floor = floor;
+            return this;
+        }
+        public String getFloors(){
+            return floor;
+        }
+
+        public HouseConstructionBuilder setRoof(String roof) {
+            this.roof = roof;
+            return this;
+        }
+
+        public HouseConstructionBuilder setDoors(int doors) {
+            this.doors = doors;
+            return this;
+        }
+
+        public HouseConstruction build(){
+
+            HouseConstruction h = new HouseConstruction();
+            h.doors = this.doors;
+            h.floor = this.floor;
+            h.roof = this.roof;
+
+
+            return h;
+        }
     }
 }
